70bf90e04e39ebe2f47d0dca7ba89b8962570b65
blazingulag <k8lorenzo@gmail.com>
2025-05-01 23:39:58 +0200

    yet another seal rework

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -692,9 +692,9 @@ return {
             },
             prism_green_seal = {
                 name = "Green Seal",
-                text = {"{C:attention}Ignores{} selection limit",
-                    "{C:green}#1# in #2#{} chance to",
-                    "destroy card",
+                text = {"{C:attention}Return{} to hand after",
+                    "being played for the {C:attention}first",
+                    "{C:attention}time{} each round"
                 }, 
             },
             prism_green_old_seal = {
